ca96b4ae3ff6dc363968c9361df3fa67140de75d
misinformation <kytsuna@gmail.com>
2019-11-20 14:01:37 +0000

    countdown timer

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const countDownDate = new Date ("Dec 31, 2019 00:00:00").getTime();
+const x = setInterval(function() {
+  const now = new Date().getTime();
+  let distance = countDownDate - now;
+    
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+ 
+  if (distance <= 0) {
+    clearInterval(x);
+  }
+}, 1000);
